0ea1da09918ba35f5912bad37e8f25a7d567a55c
Jebeton2 <hubib010@gmail.com>
2026-07-06 04:45:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,23 @@
+import discord
+from discord.ext import commands
 import os
 
 TOKEN = os.getenv("MTUxMjY1OTQyNDYwOTU3MDgxNg.Geu7j3.veeuFgpjTuDSsG2bhQ_8u35Owv5CO59FdxP8QQ")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Zalogowano jako {bot.user}")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Bot działa!")
+
 bot.run(TOKEN)
 
 intents = discord.Intents.default()
